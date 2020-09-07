c4f65bff8d2026971416d7d9de2925e90e988579
André <andre.gviva@gmail.com>
2020-09-07 19:04:05 -0300

    new feature

--- src/shared/config/database.ts
+++ src/shared/config/database.ts
@@ -33,6 +33,24 @@ const connection = () => {
         subscribersDir: process.env.DB_SUBSCRIBERS_DIR,
       },
     },
+    production: {
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: true,
+      logging: false,
+      entities: [process.env.DB_ENTITIES],
+      migrations: [process.env.DB_MIGRATIONS],
+      subscribers: [process.env.DB_SUBSCRIBERS],
+      cli: {
+        entitiesDir: process.env.DB_ENTITIES_DIR,
+        migrationsDir: process.env.DB_MIGRATIONS_DIR,
+        subscribersDir: process.env.DB_SUBSCRIBERS_DIR,
+      },
+    },
   };
 
   return databases[process.env.NODE_ENV || 'development'];
